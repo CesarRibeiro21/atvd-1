dbe4c69042c4cd8b8741f2dd145e2b2ef5736966
CesarRibeiro21 <cesartertulianort@gmail.com>
2024-08-02 11:43:35 -0300

    subindo atividade

---
+++ atvd.js
@@ -0,0 +1,18 @@
+import prompt from 'prompt-sync'
+let ler = prompt ();
+
+import express from 'express';
+
+const servidor = express();
+
+
+servidor.get('/calculadora/subtracao/:n1/:n2', (req, resp) => {
+    let n1 = Number(req.params.n1);
+    let n2 = Number(req.params.n2);
+
+    let subtracao = n1 - n2;
+
+    resp.send('A subtracao é ' + subtracao);
+})
+
+servidor.listen(5050, () => console.log('API subiu na porta 5050'));
