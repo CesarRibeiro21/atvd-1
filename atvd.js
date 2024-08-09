69dc7433403a16022c09a0b7eee3dac09ee68415
CesarRibeiro21 <cesartertulianort@gmail.com>
2024-08-09 07:17:13 -0300

    testando novas contas

--- atvd.js
+++ atvd.js
@@ -10,9 +10,9 @@ servidor.get('/calculadora/subtracao/:n1/:n2', (req, resp) => {
     let n1 = Number(req.params.n1);
     let n2 = Number(req.params.n2);
 
-    let subtracao = n1 - n2;
+    let mult = n1 * n2;
 
-    resp.send('A subtracao é ' + subtracao);
+    resp.send('A multiplicação é ' + mult);
 })
 
 servidor.listen(5050, () => console.log('API subiu na porta 5050'));
